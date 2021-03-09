450308eef9482ffe9176284130eb40fe58940662
Ngulefac <ngulefacfolefac@gmail.com>
2021-03-09 19:35:59 +0100

    Sockets

--- Blacksquad-DOS.py
+++ Blacksquad-DOS.py
@@ -11,3 +11,8 @@ minute = now.minute
 day = now.day
 month = now.month
 year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
