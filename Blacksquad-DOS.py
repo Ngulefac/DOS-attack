fe9e9355b84aa52354dea5c57d2c1754f3983b0c
Ngulefac <ngulefacfolefac@gmail.com>
2021-03-09 19:52:39 +0100

    Count down

--- Blacksquad-DOS.py
+++ Blacksquad-DOS.py
@@ -31,3 +31,14 @@ port = input("Port       : ")
 os.system("clear")
 os.system("figlet 'Attack Starting'")
 os.system("echo '       ]===> I am the Boss now <===['")
+print " \n"
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
